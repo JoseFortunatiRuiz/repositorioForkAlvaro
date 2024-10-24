86cb9c5c7203635d659e94fd71567cdcbd654738
Fortunati <143550317+JoseFortunatiRuiz@users.noreply.github.com>
2024-10-24 13:33:42 +0200

    Subida de archivos inicial

---
+++ U2EJ1_jfr.js
@@ -0,0 +1,16 @@
+// Definición de las variables numéricas
+var entero = 8965;          // Variable de tipo entero
+var decimal = 89.65;        // Variable de tipo decimal
+var cientifico = 25e5;     // Variable en notación científica
+var octal = 05762;          // Variable en octal (equivalente a 5000 en decimal)
+var hexadecimal = 0x8965;   // Variable en hexadecimal (equivalente a 35781 en decimal)
+
+// Mostrar los valores de las variables usando alertas
+alert("Número entero: " + entero);
+alert("Número decimal: " + decimal);
+alert("Número científico: " + cientifico);
+alert("Número octal: " + octal);
+alert("Número hexadecimal: " + hexadecimal);
+
+// Ejercicio: U2EJ1 - Números
+// Nombre: José Fortunati Ruiz
